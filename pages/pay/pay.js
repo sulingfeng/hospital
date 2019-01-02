f44b0a6d33c648347ed472012479b8ddb804772d
sulingfeng <30663404+sulingfeng@users.noreply.github.com>
2019-01-02 21:11:50 +0800

    支付功能
    支付功能

--- pages/pay/pay.js
+++ pages/pay/pay.js
@@ -1,5 +1,5 @@
 // pages/doctor/doctor.js
-
+const app = getApp();
 Page({
 
   /**
@@ -9,6 +9,62 @@ Page({
     current: 'tab1',
     introduce: "hidden",
     registration: "show"
+  },
+
+  WXPay:function(){
+    console.log("调用了支付接口")
+    console.log("调用了支付接口", app.globalData.userInfo)
+    wx.showToast({
+      title: '支付成功',
+      icon: 'success',
+      complete: function () {
+        // url = escape(url + "&resultCode=SUCCESS");
+        // wx.redirectTo({
+        //   url: "/pages/view/view?url=" + url + "&fnName=" + fnName
+        // })
+      }
+    })
+    return;
+
+
+    var url = "/MinProgram/PayConfirm?orderID=" + options.orderID;
+    var fnName = "/MinProgram/PayConfirm";
+    wx.requestPayment({
+      "appId": options.appId,
+      "timeStamp": options.timeStamp,
+      "nonceStr": options.nonceStr,
+      "package": 'prepay_id=' + options.prepayId,
+      "signType": options.signType,
+      "paySign": options.paySign,
+      "complete": function (res) {
+        // console.log("小程序支付成功：");
+        // console.log(res);
+        wx.showToast({
+          title: '支付成功',
+          icon: 'success',
+          complete: function () {
+            url = escape(url + "&resultCode=SUCCESS");
+            wx.redirectTo({
+              url: "/pages/view/view?url=" + url + "&fnName=" + fnName
+            })
+          }
+        })
+      },
+      'fail': function (res) {
+        // console.log("小程序支付失败：");
+        // console.log(res);
+        wx.showModal({
+          title: '支付失败',
+          content: res.errMsg,
+          complete: function () {
+            url = escape(url + "&resultCode=FAIL");
+            wx.redirectTo({
+              url: "/pages/view/view?url=" + url + "&fnName=" + fnName
+            })
+          }
+        });
+      }
+    })
   },
 
   /**
@@ -30,7 +86,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    this.switchView('week');
+    
 
   },
 
